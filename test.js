aaae6430805b050d713e272f9b43bc4f04fd9866
Abhishek Singh <abhishek.s@codilar.com>
2023-01-10 15:34:51 +0530

    Currency exchange App

---
+++ test.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch';
+const api_url ="https://zenquotes.io/api/quotes/";
+
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+}
+
+getapi(api_url);
